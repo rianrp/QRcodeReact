154b60e970dbe5e740f6cbf0f6efab4128000234
Alexandre <desenvolvedor1.linestur@gmail.com>
2022-09-27 17:18:53 -0300

    f

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import QRCode from "react-qr-code";
+import qrcodelink from "qrcode";
+import { useState } from "react";
+import { LoadingButton } from '@mui/lab/LoadingButton';
+import SaveIcon from '@mui/icons-material/Save';
+import Stack from '@mui/material/Stack';
+import { Button } from "@mui/material";
 
 function App() {
+  const [link, setLink] = useState("");
+  const [codelink, setcodelink] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const handleGerarlink = (link_url) => {
+    qrcodelink.toDataURL(
+      link_url,
+      {
+        width: 600,
+        margin: 3,
+      },
+      function (err, url) {
+        setcodelink(url);
+      }
+    );
+  };
+  const handleQRcode = (e) => {
+    setLink(e.target.value);
+    handleGerarlink(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <QRCode value={link} />
+      <input
+        className="input"
+        placeholder="Digite seu link"
+        value={link}
+        onChange={(e) => handleQRcode(e)}
+      ></input>
+      <Button isLoading={true} variant="contained" href={codelink} download={'foto.png'}>Contained</Button>
     </div>
   );
 }
